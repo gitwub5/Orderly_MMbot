d4e053115c34d36f80a1db1898ce432e58b8681e
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-08-01 16:50:41 +0900

    feat: strategies

--- src/strategy/strategies.ts
+++ src/strategy/strategies.ts
@@ -4,20 +4,20 @@ import { StrategyConfig } from "./strategyConfig";
 //Taker는 0.03%로 수수료 발생
 
 export const strategies: Record<string, StrategyConfig> = {
-    // 'PERP_TON_USDC': {
-    //     symbol: 'PERP_TON_USDC',
-    //     precision: 4,
-    //     orderQuantity: 15,
-    //     tradePeriodMs: 10000,
-    //     stdDevPeriod: 20,
-    //     orderLevels: 5,
-    //     orderSpacing: 0.02, // (0.01~0.1)
-    //     takeProfitRatio: 0.03,
-    //     stopLossRatio: 0.01,
-    //     gamma: 0.2, // 낮은 값은 더 많은 리스크를 감수하고, 높은 값은 리스크를 줄이는 경향 (0.01~0.5)
-    //     k: 6,  // 시장의 변동성과 거래 빈도에 따라 조정 (1~10)
-    //     stdDevThreshold: 0.002,
-    // },
+    'PERP_TON_USDC': {
+        symbol: 'PERP_TON_USDC',
+        precision: 4,
+        orderQuantity: 15,
+        tradePeriodMs: 10000,
+        stdDevPeriod: 20,
+        orderLevels: 5,
+        orderSpacing: 0.02, // (0.01~0.1)
+        takeProfitRatio: 0.03,
+        stopLossRatio: 0.01,
+        gamma: 0.2, // 낮은 값은 더 많은 리스크를 감수하고, 높은 값은 리스크를 줄이는 경향 (0.01~0.5)
+        k: 6,  // 시장의 변동성과 거래 빈도에 따라 조정 (1~10)
+        stdDevThreshold: 0.002,
+    },
     
     'PERP_LINK_USDC': {
         symbol: 'PERP_LINK_USDC',
@@ -27,10 +27,10 @@ export const strategies: Record<string, StrategyConfig> = {
         stdDevPeriod: 30,
         orderLevels: 3,
         orderSpacing: 0.05,
-        takeProfitRatio: 0.04,
-        stopLossRatio: 0.02,
+        takeProfitRatio: 0.03,
+        stopLossRatio: 0.01,
         gamma: 0.2,
-        k: 5,
+        k: 4,
         stdDevThreshold: 0.02,
     },
 
