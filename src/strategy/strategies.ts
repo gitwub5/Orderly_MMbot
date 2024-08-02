9ef7902ec2544980ad657379b7ec50f62baf3f45
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-08-02 23:13:35 +0900

    update: strategy3

--- src/strategy/strategies.ts
+++ src/strategy/strategies.ts
@@ -14,8 +14,8 @@ export const strategies: Record<string, StrategyConfig> = {
         orderSpacing: 0.01, // (0.01~0.1)
         takeProfitRatio: 0.02,
         stopLossRatio: 0.01,
-        gamma: 0.3, // 낮은 값은 더 많은 리스크를 감수하고, 높은 값은 리스크를 줄이는 경향 (0.01~0.5)
-        k: 5.2,  // 시장의 변동성과 거래 빈도에 따라 조정 (1~10)
+        gamma: 0.5, // 낮은 값은 더 많은 리스크를 감수하고, 높은 값은 리스크를 줄이는 경향 (0.01~0.5)
+        k: 5.8,  // 시장의 변동성과 거래 빈도에 따라 조정 (1~10)
         stdDevThreshold: 0.002,
     },
     
@@ -29,8 +29,8 @@ export const strategies: Record<string, StrategyConfig> = {
         orderSpacing: 0.035,
         takeProfitRatio: 0.02,
         stopLossRatio: 0.01,
-        gamma: 0.4,
-        k: 5.7,
+        gamma: 0.5,
+        k: 6,
         stdDevThreshold: 0.002,
     },
 
@@ -45,7 +45,7 @@ export const strategies: Record<string, StrategyConfig> = {
         takeProfitRatio: 0.02,
         stopLossRatio: 0.01,
         gamma: 0.5,
-        k: 0.8,
+        k: 1,
         stdDevThreshold: 0.05,
     },
 
@@ -59,8 +59,8 @@ export const strategies: Record<string, StrategyConfig> = {
         orderSpacing: 0.01,
         takeProfitRatio: 0.02,
         stopLossRatio: 0.01,
-        gamma: 0.2,
-        k: 140,
+        gamma: 0.4,
+        k: 150,
         stdDevThreshold: 0.00003,
     },
 
